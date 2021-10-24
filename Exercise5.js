e8b4a1dfae580d81ac1c73ae1d1ef787f81cfe1d
Alberto Castro Olea <alberto.reed@hotmail.es>
2021-10-24 21:18:00 +0200

    Implemented fifth exercise

---
+++ Exercise5.js
@@ -0,0 +1,21 @@
+class SlotMachine {
+    constructor() {
+        this.coins = 0;
+    }
+
+    play() {
+        this.coins++;
+
+        let roulettes = true;
+        for (let i = 0; i < 3; i++) {
+            roulettes &= Math.random() < 0.5;
+            if (!roulettes) {
+                console.log('Good luck next time!!');
+                return;
+            }
+        }
+
+        console.log(`Congratulations!!! You won ${this.coins} coins!!`);
+        this.coins = 0;
+    }
+}
